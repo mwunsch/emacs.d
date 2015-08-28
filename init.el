92d0b2d673676fdc72c2b55e5b62f7c6f2074264
Mark Wunsch <mwunsch@renttherunway.com>
2015-08-28 15:32:24 -0400

    Organize mode enabling and keybindings.
    Enable company, auto-revert, and delete-selection modes. Require a
    final newline on save.

--- init.el
+++ init.el
@@ -28,32 +28,30 @@
 (when (eq window-system 'ns)
   (exec-path-from-shell-initialize))
 
-;; Mappings so that ⌘-+, ⌘--, ⌘-0 adjust font size like other Mac apps.
-;; The 'text-scale-adjust function knows what to do.
-(dolist (key '("s-0" "s-=" "s-+" "s--"))
-  (global-set-key (kbd key) 'text-scale-adjust))
-
-(global-set-key (kbd "M-p") 'ace-window)
-
 (global-prettify-symbols-mode t)
 (projectile-global-mode)
-
+(global-company-mode t)
+(flx-ido-mode 1)
+(ido-vertical-mode 1)
+(global-anzu-mode 1)
+(global-auto-revert-mode t)
 ;; Set up keybindings in file-visiting buffers for magit
 ;;   C-x g    'magit-status
 ;;   C-c M-g  'magit-file-buffer-popup
 ;;   C-x M-g  'magit-dispatch-popup
 (global-magit-file-buffer-mode 1)
+(delete-selection-mode t)
 
 ;; Set company mode so that it is invoked on TAB.
 (setq company-idle-delay nil)
-(global-set-key (kbd "TAB") #'company-indent-or-complete-common)
+(setq require-final-newline t)
 
-(flx-ido-mode 1)
-(ido-vertical-mode 1)
-(global-anzu-mode 1)
+(global-set-key (kbd "M-p") 'ace-window)
+(global-set-key (kbd "TAB") 'company-indent-or-complete-common)
+;; Mappings so that ⌘-+, ⌘--, ⌘-0 adjust font size like other Mac apps.
+;; The 'text-scale-adjust function knows what to do.
+(dolist (key '("s-0" "s-=" "s-+" "s--"))
+  (global-set-key (kbd key) 'text-scale-adjust))
 
 (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
 (add-hook 'clojure-mode-hook #'paredit-mode)
-
-
-
