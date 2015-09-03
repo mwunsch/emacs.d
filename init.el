066095ec2f24b13da42ff05e4a3016393c2b527a
Mark Wunsch <mwunsch@renttherunway.com>
2015-09-03 15:59:27 -0400

    Use emacs-mac port, and make sure Command is Super

--- init.el
+++ init.el
@@ -61,3 +61,17 @@
 (add-hook 'clojure-mode-hook #'paredit-mode)
 (add-hook 'prog-mode-hook #'linum-mode)
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mac-command-modifier (quote super))
+ '(mac-option-modifier (quote meta)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
