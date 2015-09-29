c2590eb09865deb5ac4b81d355e1e0cf68bb1d74
Mark Wunsch <mwunsch@renttherunway.com>
2015-09-29 18:11:10 -0400

    Attempt to add hook for text notifications on ERC

--- .ercrc.el
+++ .ercrc.el
@@ -2,3 +2,11 @@
 
 (setq erc-autojoin-channels-alist '(("freenode.net" "#icymi")))
 (setq erc-nick "mwunsch")
+
+;;; Like erc notifications mode, but for OSX.
+;;; Unsure if ercrc.el is the best place for this, and it's just a first stab...
+(when (fboundp 'osx-notify)
+  (add-hook 'erc-text-matched-hook (lambda (match-type nickuserhost msg)
+                                     (when (eq match-type 'current-nick)
+                                       (let ((nick (nth 0 (erc-parse-user nickuserhost))))
+                                         (osx-notify (format "ERC: %s" nick) msg))))))
