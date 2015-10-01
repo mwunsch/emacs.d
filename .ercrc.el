403ca76f153dc6f38db6701a12e35cd7803b057a
Mark Wunsch <mwunsch@renttherunway.com>
2015-10-01 11:21:39 -0400

    Get my erc notifications game right for OSX

--- .ercrc.el
+++ .ercrc.el
@@ -2,3 +2,13 @@
 
 (setq erc-autojoin-channels-alist '(("freenode.net" "#icymi")))
 (setq erc-nick "mwunsch")
+
+;;; Like erc notifications mode, but for OSX.
+(when (fboundp 'osx-notify)
+  (add-hook 'erc-text-matched-hook (lambda (match-type nickuserhost msg)
+                                     (when (eq match-type 'current-nick)
+                                       (let ((nick (nth 0 (erc-parse-user nickuserhost))))
+                                         (when (and (not (string-match-p "^Server:" nick))
+                                                    (not (string-match-p "^NickServ" nick)))
+                                           (erc-remove-text-properties-region 0 (length msg) msg)
+                                           (osx-notify (format "ERC: %s" nick) msg)))))))
