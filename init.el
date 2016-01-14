96eaeafe8e24f014f0e8835286213f9567b5887d
Mark Wunsch <mwunsch@renttherunway.com>
2016-01-14 18:00:29 -0500

    Always pretty print in CIDER repl

--- init.el
+++ init.el
@@ -73,6 +73,7 @@
                                     'split-window-vertically))
 (setq coffee-tab-width 2)
 (setq cider-repl-tab-command 'company-indent-or-complete-common)
+(setq cider-repl-use-pretty-printing t)
 (setq css-indent-offset 2)
 
 ;;; I send mail with fastmail.fm!
