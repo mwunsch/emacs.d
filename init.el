4c08c3943c026aaf3d15d7670d49adc341b72129
Mark Wunsch <mark@markwunsch.com>
2016-05-10 12:16:41 -0400

    Make certain variables safe for dirlocal

--- init.el
+++ init.el
@@ -145,6 +145,11 @@
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("Appfile\\'" . hcl-mode))
 
+(put 'css-indent-offset 'safe-local-variable #'integerp)
+(put 'sgml-basic-offset 'safe-local-variable #'integerp)
+(put 'js-indent-level 'safe-local-variable #'integerp)
+(put 'whitespace-line-column 'safe-local-variable #'integerp)
+
 (setq solarized-distinct-fringe-background t)
 (setq solarized-high-contrast-mode-line t)
 (setq solarized-use-more-italic t)
