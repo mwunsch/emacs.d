d4816ae783717b5c92c982fe19b6dba47f5c8114
Mark Wunsch <mark@markwunsch.com>
2017-01-12 12:30:01 -0500

    All html files should be in web-mode

--- init.el
+++ init.el
@@ -154,6 +154,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("Appfile\\'" . hcl-mode))
 
 (add-to-list 'company-backends 'company-elm)
